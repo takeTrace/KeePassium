9a4b60eed5864fcf72f90f45180e94e55bb2d226
KeePassium <info@keepassium.com>
2019-06-26 23:04:09 +0200

    refine: add swiping hint rebound
    Now it bumps twice, looks better.

--- KeePassium/util/UITableViewCell+demoEditActions.swift
+++ KeePassium/util/UITableViewCell+demoEditActions.swift
@@ -9,17 +9,17 @@
 import UIKit
 
 extension UITableViewCell {
-    
+
     /// Shows animation hinting about left-swipe actions.
     ///
     /// - Parameter lastActionColor: color of the rightmost action.
     public func demoShowEditActions(lastActionColor: UIColor) {
         guard let cellView = contentView.superview else { return }
-        
-        let shiftBy: CGFloat = 22 // by how many points to shift the cell
-        
         let wasClippingToBounds = cellView.clipsToBounds
         cellView.clipsToBounds = false
+
+        let maxShift = cellView.frame.height / 2 // by how many points to shift the cell
+
         let fakeActionView = UIView(frame: self.contentView.bounds)
         fakeActionView.backgroundColor = .destructiveTint
         contentView.addSubview(fakeActionView)
@@ -27,33 +27,40 @@ extension UITableViewCell {
         fakeActionView.topAnchor.constraint(equalTo: cellView.topAnchor).isActive = true
         fakeActionView.bottomAnchor.constraint(equalTo: cellView.bottomAnchor).isActive = true
         fakeActionView.leadingAnchor.constraint(equalTo: cellView.trailingAnchor).isActive = true
-        fakeActionView.widthAnchor.constraint(equalToConstant: shiftBy).isActive = true
+        fakeActionView.widthAnchor.constraint(equalToConstant: maxShift).isActive = true
         fakeActionView.isOpaque = true
         fakeActionView.layoutIfNeeded()
         
-        let originalFrame = cellView.frame
-        let shiftedFrame = cellView.frame.offsetBy(dx: -shiftBy, dy: 0)
-        UIView.animate(
-            withDuration: 0.3,
-            delay: 0.0,
-            options: [.curveEaseOut],
-            animations: {
-                cellView.frame = shiftedFrame
-            },
-            completion: { (finished) in
-                UIView.animate(
-                    withDuration: 0.3,
-                    delay: 0.0,
-                    options: [.curveEaseIn],
-                    animations: {
-                        cellView.frame = originalFrame
-                    },
-                    completion: { (finished) in
+        animateFrameShift(cellView: cellView, by: -maxShift) {
+            [weak self] in
+            self?.animateFrameShift(cellView: cellView, by: maxShift) {
+                [weak self] in
+                self?.animateFrameShift(cellView: cellView, by: -0.3 * maxShift) {
+                    [weak self] in
+                    self?.animateFrameShift(cellView: cellView, by: 0.3 * maxShift) {
                         fakeActionView.removeFromSuperview()
                         cellView.clipsToBounds = wasClippingToBounds
                     }
-                )
+                }
             }
-        )
+        }
+    }
+    
+    private func animateFrameShift(
+        cellView: UIView,
+        by dx: CGFloat,
+        completion: @escaping ()->Void)
+    {
+        
+        let shiftedFrame = cellView.frame.offsetBy(dx: dx, dy: 0.0)
+        let options: UIView.AnimationOptions = (dx > 0) ? [.curveEaseIn] : [.curveEaseOut]
+        UIView.animate(
+            withDuration: 0.25,
+            delay: 0.0,
+            options: options,
+            animations: {
+                cellView.frame = shiftedFrame
+            },
+            completion: { (finished) in completion() })
     }
 }
