15d5925504c76e9b552cf7b22e1281bbc4f3bcc0
KeePassium <info@keepassium.com>
2019-07-18 15:16:01 +0200

    feat(premium): separate free/premium support emails

--- KeePassium/util/SupportEmailComposer.swift
+++ KeePassium/util/SupportEmailComposer.swift
@@ -1,6 +1,6 @@
 //  KeePassium Password Manager
 //  Copyright © 2018–2019 Andrei Popleteev <info@keepassium.com>
-// 
+//
 //  This program is free software: you can redistribute it and/or modify it
 //  under the terms of the GNU General Public License version 3 as published
 //  by the Free Software Foundation: https://www.gnu.org/licenses/).
@@ -12,7 +12,8 @@ import KeePassiumLib
 
 /// Helper class to create support email templates.
 class SupportEmailComposer: NSObject {
-    private let supportEmail = "support@keepassium.com"
+    private let freeSupportEmail = "support@keepassium.com"
+    private let premiumSupportEmail = "premium-support@keepassium.com"
     
     typealias CompletionHandler = ((Bool)->Void)
     private let completionHandler: CompletionHandler?
@@ -54,26 +55,39 @@ class SupportEmailComposer: NSObject {
         let instance = SupportEmailComposer(subject: subject, content: content,
                                             completionHandler: completion)
         
-//        if MFMailComposeViewController.canSendMail() {
-//            instance.showEmailComposer()
-//        } else {
-//            instance.openSystemEmailComposer()
-//        }
+        //        if MFMailComposeViewController.canSendMail() {
+        //            instance.showEmailComposer()
+        //        } else {
+        //            instance.openSystemEmailComposer()
+        //        }
         // In-app composer does not show up on iOS11+, thus mailto workaround
         instance.openSystemEmailComposer()
+    }
+    
+    private func getSupportEmail() -> String {
+        let premiumStatus = PremiumManager.shared.status
+        switch premiumStatus {
+        case .initialGracePeriod,
+             .freeLightUse,
+             .freeHeavyUse:
+            return freeSupportEmail
+        case .subscribed,
+             .lapsed:
+            return premiumSupportEmail
+        }
     }
     
     private func showEmailComposer() {
         let emailComposerVC = MFMailComposeViewController()
         emailComposerVC.mailComposeDelegate = self
-        emailComposerVC.setToRecipients([supportEmail])
+        emailComposerVC.setToRecipients([getSupportEmail()])
         emailComposerVC.setSubject(subject)
         emailComposerVC.setMessageBody(content, isHTML: false)
     }
     
     private func openSystemEmailComposer() {
         let body = content.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)! // safe to unwrap
-        let mailtoUrl = "mailto:\(supportEmail)?subject=\(subject)&body=\(body)"
+        let mailtoUrl = "mailto:\(getSupportEmail())?subject=\(subject)&body=\(body)"
         guard let url = URL(string: mailtoUrl) else {
             Diag.error("Failed to create mailto URL")
             return
