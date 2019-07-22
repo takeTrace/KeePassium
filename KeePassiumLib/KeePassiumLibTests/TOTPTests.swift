640a0595dc3e3a1c55301d19eca76219a931ff61
KeePassium <info@keepassium.com>
2019-07-22 07:43:53 +0200

    test(totp): add tests for KeeOtp format parsing

--- KeePassiumLib/KeePassiumLibTests/TOTPTests.swift
+++ KeePassiumLib/KeePassiumLibTests/TOTPTests.swift
@@ -17,11 +17,22 @@ class TOTPTests: XCTestCase {
         seed = ByteArray(bytes: base32Decode(seedString)!)
     }
     
-    // MARK: - URI format
+    private func asFields(_ settingsString: String) -> [EntryField] {
+        return [EntryField(name: "otp", value: settingsString, isProtected: true)]
+    }
+    
+    private func asFields(_ seedString: String, _ settingsString: String) -> [EntryField] {
+        return [
+            EntryField(name: "TOTP Seed", value: seedString, isProtected: true),
+            EntryField(name: "TOTP Settings", value: settingsString, isProtected: false)
+        ]
+    }
+    
+    // MARK: - Google Auth format
     
     func test1() {
         let uri = "otpauth://totp/ACME%20Co:john.doe@email.com?secret=HXDMVJECJJWSRB3HWIZR4IFUGFTMXBOZ&issuer=ACME%20Co&algorithm=SHA1&digits=4&period=60"
-        let gen = TOTPGeneratorFactory.makeGenerator(uri: uri) as? TOTPGeneratorRFC6238
+        let gen = TOTPGeneratorFactory.makeGenerator(from: asFields(uri)) as? TOTPGeneratorRFC6238
         
         if gen == nil { XCTFail(); return }
         XCTAssert(gen!.seed == seed)
@@ -31,7 +42,7 @@ class TOTPTests: XCTestCase {
 
     func test1a() {
         let uri = "otpauth://totp/Example:alice@google.com?secret=JBSWY3DPEHPK3PXP&issuer=Example"
-        let gen = TOTPGeneratorFactory.makeGenerator(uri: uri) as? TOTPGeneratorRFC6238
+        let gen = TOTPGeneratorFactory.makeGenerator(from: asFields(uri)) as? TOTPGeneratorRFC6238
         
         if gen == nil { XCTFail(); return }
         let trueSeed = ByteArray(bytes: base32Decode("JBSWY3DPEHPK3PXP")!)
@@ -42,7 +53,7 @@ class TOTPTests: XCTestCase {
 
     func test2() {
         let uri = "otpauth://totp/ACME%20Co:john.doe@email.com?secret=HXDMVJECJJWSRB3HWIZR4IFUGFTMXBOZ&issuer=ACME%20Co"
-        let gen = TOTPGeneratorFactory.makeGenerator(uri: uri) as? TOTPGeneratorRFC6238
+        let gen = TOTPGeneratorFactory.makeGenerator(from: asFields(uri)) as? TOTPGeneratorRFC6238
         
         if gen == nil { XCTFail(); return }
         XCTAssert(gen!.seed == seed)
@@ -52,7 +63,7 @@ class TOTPTests: XCTestCase {
 
     func test3() {
         let uri = "otpauth://totp/?secret=HXDMVJECJJWSRB3HWIZR4IFUGFTMXBOZ&digits=8&period=123"
-        let gen = TOTPGeneratorFactory.makeGenerator(uri: uri) as? TOTPGeneratorRFC6238
+        let gen = TOTPGeneratorFactory.makeGenerator(from: asFields(uri)) as? TOTPGeneratorRFC6238
         
         if gen == nil { XCTFail(); return }
         XCTAssert(gen!.seed == seed)
@@ -62,7 +73,7 @@ class TOTPTests: XCTestCase {
     
     func test4() {
         let uri = "otpauth://totp/?secret=HXDMVJECJJWSRB3HWIZR4IFUGFTMXBOZ"
-        let gen = TOTPGeneratorFactory.makeGenerator(uri: uri) as? TOTPGeneratorRFC6238
+        let gen = TOTPGeneratorFactory.makeGenerator(from: asFields(uri)) as? TOTPGeneratorRFC6238
         
         if gen == nil { XCTFail(); return }
         XCTAssert(gen!.seed == seed)
@@ -72,59 +83,117 @@ class TOTPTests: XCTestCase {
     
     func testMisformat1() {
         let uri = "hello world"
-        let gen = TOTPGeneratorFactory.makeGenerator(uri: uri) as? TOTPGeneratorRFC6238
+        let gen = TOTPGeneratorFactory.makeGenerator(from: asFields(uri)) as? TOTPGeneratorRFC6238
         XCTAssertNil(gen)
     }
     func testMisformat2() {
         let uri = "otpauth://something"
-        let gen = TOTPGeneratorFactory.makeGenerator(uri: uri) as? TOTPGeneratorRFC6238
+        let gen = TOTPGeneratorFactory.makeGenerator(from: asFields(uri)) as? TOTPGeneratorRFC6238
         XCTAssertNil(gen)
     }
     func testMisformat3() {
         let uri = "otpauth://hotp"
-        let gen = TOTPGeneratorFactory.makeGenerator(uri: uri) as? TOTPGeneratorRFC6238
+        let gen = TOTPGeneratorFactory.makeGenerator(from: asFields(uri)) as? TOTPGeneratorRFC6238
         XCTAssertNil(gen)
     }
     func testMisformat4() {
         let uri = "otpauth://totp"
-        let gen = TOTPGeneratorFactory.makeGenerator(uri: uri) as? TOTPGeneratorRFC6238
+        let gen = TOTPGeneratorFactory.makeGenerator(from: asFields(uri)) as? TOTPGeneratorRFC6238
         XCTAssertNil(gen)
     }
     func testMisformat5() {
         let uri = "otpauth://totp/?secret="
-        let gen = TOTPGeneratorFactory.makeGenerator(uri: uri) as? TOTPGeneratorRFC6238
+        let gen = TOTPGeneratorFactory.makeGenerator(from: asFields(uri)) as? TOTPGeneratorRFC6238
         XCTAssertNil(gen)
     }
     func testMisformat6() {
         let uri = "otpauth://totp/?secret=WRONG_ONE"
-        let gen = TOTPGeneratorFactory.makeGenerator(uri: uri) as? TOTPGeneratorRFC6238
+        let gen = TOTPGeneratorFactory.makeGenerator(from: asFields(uri)) as? TOTPGeneratorRFC6238
         XCTAssertNil(gen)
     }
     func testMisformat7() {
         let uri = "otpauth://totp?secret=HXDMVJECJJWSRB3HWIZR4IFUGFTMXBOZ&algorithm=SHA512"
-        let gen = TOTPGeneratorFactory.makeGenerator(uri: uri) as? TOTPGeneratorRFC6238
+        let gen = TOTPGeneratorFactory.makeGenerator(from: asFields(uri)) as? TOTPGeneratorRFC6238
         XCTAssertNil(gen)
     }
     func testMisformat8() {
         let uri = "otpauth://totp/ACME%20Co:john.doe@email.com?secret=HXDMVJECJJWSRB3HWIZR4IFUGFTMXBOZ&algorithm=SHA1&digits=BBB"
-        let gen = TOTPGeneratorFactory.makeGenerator(uri: uri) as? TOTPGeneratorRFC6238
+        let gen = TOTPGeneratorFactory.makeGenerator(from: asFields(uri)) as? TOTPGeneratorRFC6238
         XCTAssertNil(gen)
     }
     func testMisformat9() {
         let uri = "otpauth://totp/ACME%20Co:john.doe@email.com?secret=HXDMVJECJJWSRB3HWIZR4IFUGFTMXBOZ&period=AAA"
-        let gen = TOTPGeneratorFactory.makeGenerator(uri: uri) as? TOTPGeneratorRFC6238
+        let gen = TOTPGeneratorFactory.makeGenerator(from: asFields(uri)) as? TOTPGeneratorRFC6238
         XCTAssertNil(gen)
     }
     func testMisformat10() {
         let uri = "otpauth://totp/ACME%20Co:john.doe@email.com?secret=HXDMVJECJJWSRB3HWIZR4IFUGFTMXBOZ&digits=&period="
-        let gen = TOTPGeneratorFactory.makeGenerator(uri: uri) as? TOTPGeneratorRFC6238
+        let gen = TOTPGeneratorFactory.makeGenerator(from: asFields(uri)) as? TOTPGeneratorRFC6238
         XCTAssertNil(gen)
     }
+    
+    // MARK: - KeeOtp format
+    func testKeeOtpFormat1() {
+        let uri = "key=HXDMVJECJJWSRB3HWIZR4IFUGFTMXBOZ"
+        let gen = TOTPGeneratorFactory.makeGenerator(from: asFields(uri)) as? TOTPGeneratorRFC6238
+        
+        if gen == nil { XCTFail(); return }
+        XCTAssert(gen!.seed == seed)
+        XCTAssert(gen!.length == 6)
+        XCTAssert(gen!.timeStep == 30)
+    }
+    
+    func testKeeOtpFormat2() {
+        let uri = "key=HXDMVJECJJWSRB3HWIZR4IFUGFTMXBOZ&size=8&step=123"
+        let gen = TOTPGeneratorFactory.makeGenerator(from: asFields(uri)) as? TOTPGeneratorRFC6238
+        
+        if gen == nil { XCTFail(); return }
+        XCTAssert(gen!.seed == seed)
+        XCTAssert(gen!.length == 8)
+        XCTAssert(gen!.timeStep == 123)
+    }
+    
+    func testKeeOtpFormat3() {
+        let uri = "key=HXDMVJECJJWSRB3HWIZR4IFUGFTMXBOZ&size=8&step=123&type=totp&otpHashMode=SHA1"
+        let gen = TOTPGeneratorFactory.makeGenerator(from: asFields(uri)) as? TOTPGeneratorRFC6238
+        
+        if gen == nil { XCTFail(); return }
+        XCTAssert(gen!.seed == seed)
+        XCTAssert(gen!.length == 8)
+        XCTAssert(gen!.timeStep == 123)
+    }
+
+    func testKeeOtpMisformat1() {
+        let uri = "key=HXDMVJECJJWSRB3HWIZR4IFUGFTMXBOZ&size=8&step=123&type=totp&otpHashMode=SHA256"
+        let gen = TOTPGeneratorFactory.makeGenerator(from: asFields(uri)) as? TOTPGeneratorRFC6238
+        XCTAssertNil(gen)
+    }
+    func testKeeOtpMisformat2() {
+        let uri = "key=HXDMVJECJJWSRB3HWIZR4IFUGFTMXBOZ&size=8&step=123&type=hotp"
+        let gen = TOTPGeneratorFactory.makeGenerator(from: asFields(uri)) as? TOTPGeneratorRFC6238
+        XCTAssertNil(gen)
+    }
+    func testKeeOtpMisformat3() {
+        let uri = "key=HXDMVJECJJWSRB3HWIZR4IFUGFTMXBOZ&size=8&step=-123"
+        let gen = TOTPGeneratorFactory.makeGenerator(from: asFields(uri)) as? TOTPGeneratorRFC6238
+        XCTAssertNil(gen)
+    }
+    func testKeeOtpMisformat4() {
+        let uri = "key=HXDMVJECJJWSRB3HWIZR4IFUGFTMXBOZ&size=-1&step=123"
+        let gen = TOTPGeneratorFactory.makeGenerator(from: asFields(uri)) as? TOTPGeneratorRFC6238
+        XCTAssertNil(gen)
+    }
+    func testKeeOtpMisformat5() {
+        let uri = "key=NOT_A_VALID_BASE32"
+        let gen = TOTPGeneratorFactory.makeGenerator(from: asFields(uri)) as? TOTPGeneratorRFC6238
+        XCTAssertNil(gen)
+    }
+
     
     // MARK: - Split format
 
     func testSplitFormat1() {
-        let gen = TOTPGeneratorFactory.makeGenerator(seed: seedString, settings: "33;6")
+        let gen = TOTPGeneratorFactory.makeGenerator(from: asFields(seedString, "33;6"))
             as? TOTPGeneratorRFC6238
         if gen == nil { XCTFail(); return }
         XCTAssert(gen!.seed == seed)
@@ -133,7 +202,7 @@ class TOTPTests: XCTestCase {
     }
 
     func testSplitFormat2() {
-        let gen = TOTPGeneratorFactory.makeGenerator(seed: seedString, settings: "20;8;http://example.com")
+        let gen = TOTPGeneratorFactory.makeGenerator(from: asFields(seedString, "20;8;http://example.com"))
             as? TOTPGeneratorRFC6238
         if gen == nil { XCTFail(); return }
         XCTAssert(gen!.seed == seed)
@@ -142,17 +211,17 @@ class TOTPTests: XCTestCase {
     }
 
     func testSplitMisformat1() {
-        let gen = TOTPGeneratorFactory.makeGenerator(seed: "not_a_valid_seed", settings: "20;6")
+        let gen = TOTPGeneratorFactory.makeGenerator(from: asFields("not_a_valid_seed", "20;6"))
             as? TOTPGeneratorRFC6238
         XCTAssertNil(gen)
     }
     func testSplitMisformat2() {
-        let gen = TOTPGeneratorFactory.makeGenerator(seed: seedString, settings: "20;")
+        let gen = TOTPGeneratorFactory.makeGenerator(from: asFields(seedString, "20;"))
             as? TOTPGeneratorRFC6238
         XCTAssertNil(gen)
     }
     func testSplitMisformat3() {
-        let gen = TOTPGeneratorFactory.makeGenerator(seed: seedString, settings: "-1;8")
+        let gen = TOTPGeneratorFactory.makeGenerator(from: asFields(seedString, "-1;8"))
             as? TOTPGeneratorRFC6238
         XCTAssertNil(gen)
     }
