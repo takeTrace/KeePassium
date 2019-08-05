4a6c1b4777ee0d8d455c0fc42083419b886d55ee
KeePassium <info@keepassium.com>
2019-08-05 21:18:16 +0200

    feat: hide visible passwords when leaving the app (closes #43)

--- KeePassium/views/ProtectedTextField.swift
+++ KeePassium/views/ProtectedTextField.swift
@@ -45,8 +45,20 @@ class ProtectedTextField: ValidatingTextField {
         toggleButton.accessibilityLabel = "Show Password".localized(comment: "Button to make password visible as plain-text")
         self.rightView = toggleButton
         self.rightViewMode = .always
+        
+        // hide everything when leaving to background
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(resetVisibility(_:)),
+            name: UIApplication.willResignActiveNotification,
+            object: nil)
     }
 
+    @objc
+    func resetVisibility(_ sender: Any) {
+        isSecureTextEntry = true
+    }
+    
     @objc
     func toggleVisibility(_ sender: Any) {
         isSecureTextEntry = !isSecureTextEntry
